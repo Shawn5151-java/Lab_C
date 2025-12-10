dbedf43d8f979e2dac3cbaf7e158cb8bca53dad0
Shawn5151-java <yu263369@gmail.com>
2025-12-10 21:00:59 +0800

    修正CarModel版面

---
+++ js/Car_model.js
@@ -0,0 +1,25 @@
+function ordinary(){
+    Brand["Toyota"],CarModel["Altis","Vios","Yaris"];
+    Brand["Mazda"],CarModel["Mazda 3"]
+    Brand["Ford"],CarModel["Focus Wagon"]
+    Brand["Honda"],CarModel["Fit","Civic"]
+    Brand["Nissan"],CarModel["Sentra"]
+}
+
+function luxury(){
+    Brand["Benz"],CarModel["C300","C63","S350"]
+    Brand["BMW"],CarModel["520i","320i"]
+    Brand["Porsche"],CarModel["911","Panamera"]
+}
+
+
+function suv(){
+    Brand["Toyota"],CarModel["Cross","Rav4"]
+    Brand["Lexus"],CarModel["RX350"]
+    Brand["Ford"],CarModel["Kuga"]
+}
+
+function ev(){
+    Brand["Tesla"],CarModel["Model_3"]
+    Brand["Audi"],CarModel["e-tron_GT"]
+}
